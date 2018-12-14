be5cf1d489861df95b05b8aa9446c4a0376b7425
Nikolay Volodin (Wunderman Production) <Nikolay.Volodin@wunderman.com>
2018-12-14 13:51:18 +0100

    Refactored js with XMLHttpRequest

--- js/file_download_trigger.js
+++ js/file_download_trigger.js
@@ -9,8 +9,34 @@
 
       // Show the file download window only on full document being loaded
       if (context === document) {
-        // In Firefox it doesn't load a favicon before a file download window gets pop up
-        window.location.replace('/zip_download/' + drupalSettings.csvDataDownload.downloadZip);
+
+        var request = new XMLHttpRequest();
+        request.open('POST', '/zip_download/' + drupalSettings.csvDataDownload.downloadZip, true);
+        request.responseType = 'blob';
+
+        request.onload = function() {
+          if(request.status === 200) {
+
+            // Try to find out the filename from the content disposition `filename` value
+            var disposition = request.getResponseHeader('content-disposition');
+            var matches = /"([^"]*)"/.exec(disposition);
+            var filename = (matches != null && matches[1] ? matches[1] : drupalSettings.csvDataDownload.downloadZip + '.zip');
+
+            // The actual download
+            var blob = new Blob([request.response], { type: request.getResponseHeader('Content-Type') });
+
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = filename;
+
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+          }
+
+        };
+        request.send();
+
       }
 
     }
